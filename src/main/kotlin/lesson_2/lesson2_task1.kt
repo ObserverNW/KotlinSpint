f8925e705fa6ee0a0d738fe98a5abed1abd675df
observer <patron1402@gmail.com>
2025-05-02 22:50:34 +0300

    KS-2-1 task_1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val a: Int = 3
+    val b: Int = 4
+    val c: Int = 3
+    val d: Int = 5
+
+    val averageNum: Float = (a + b + c + d) / 4f
+    print(averageNum)
+}
