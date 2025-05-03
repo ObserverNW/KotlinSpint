85e83997e8f5b3dc4f06a44fde615bf5ec52e5a8
observer <patron1402@gmail.com>
2025-05-03 15:46:09 +0300

    KS-2-5 task_5

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -8,8 +8,8 @@ fun main() {
     val percent: Double = 0.167
     val years: Int = 20;
 
-    val finalDeposit = (deposit * Math.pow(1+percent, years.toDouble()))
+    val finalDeposit = deposit * Math.pow(1+percent, years.toDouble())
+
 //    Долго разбирался, почему не через точку выводится
     println("%.3f".format(Locale.US, finalDeposit))
-
 }
