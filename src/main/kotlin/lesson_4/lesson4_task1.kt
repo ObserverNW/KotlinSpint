153ca08acb8042682765dd6662ff4f77d6f62f77
observer <patron1402@gmail.com>
2025-05-04 14:33:06 +0300

    KS-3-1 task_1 fixed_1

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_4
+
+fun main() {
+    val bookingTableToday: Int = 13
+    val bookingTableTomorrow: Int = 9
+
+    println("[Доступность столиков на сегодня: ${ALL_TABLE > bookingTableToday}]," +
+            "\n[Доступность столиков на завтра: ${ALL_TABLE  > bookingTableTomorrow}].")
+}
+const val ALL_TABLE: Int = 13
