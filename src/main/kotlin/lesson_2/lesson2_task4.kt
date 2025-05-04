4daeae5230ac141cc7b9f946ccb08361167b1e85
observer <patron1402@gmail.com>
2025-05-04 14:45:13 +0300

    KS-2-1 task_4 fixed first

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,10 +4,10 @@ fun main() {
 
     val crystalMining: Int = 7
     val ironMining: Int = 11
-    val miningBuff: Float = 0.20f
+    val miningBuff: Int = 20
 
-    val crystalBonus = (crystalMining * miningBuff).toInt()
-    val ironBonus = (ironMining * miningBuff).toInt()
+    val crystalBonus = (crystalMining * miningBuff / 100).toInt()
+    val ironBonus = (ironMining * miningBuff / 100).toInt()
 
     println("Кристаллы с баффом: $crystalBonus")
     println("Желелезо с баффом: $ironBonus")
