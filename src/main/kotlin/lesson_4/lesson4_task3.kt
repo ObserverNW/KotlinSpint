f830aa3e5bc9e2306a7689c63696a3fbfe7596e4
observer <patron1402@gmail.com>
2025-05-04 19:48:40 +0300

    KS-4-3 task_3 myFix

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,7 +1,6 @@
 package org.example.lesson_4
 
 fun main() {
-
     val sunWeather: Boolean = true
     val openTent: Boolean = true
     val airHumidity: Int = 20
@@ -10,7 +9,4 @@ fun main() {
     val checkAllPoints = sunWeather && openTent && airHumidity == 20 && currentTime != "winter"
 
     println("Благоприятные ли условия сейчас для роста бобовых? $checkAllPoints")
-
-
-
 }
