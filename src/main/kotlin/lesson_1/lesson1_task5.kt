7458c87c03487cd64612a5b77978cccde0fe3674
observer <patron1402@gmail.com>
2025-05-08 15:27:30 +0300

    KS-1-5 fixed_2

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,14 +3,14 @@ package org.example.lesson_1
 fun main() {
     val allSeconds: Int = 6480
 
-    val hoursInSpace: Int = allSeconds / (secInMin * secInMin)
-    val remainingSeconds: Int = allSeconds % (secInMin * secInMin)
+    val hoursInSpace: Int = allSeconds / (SEC_IN_MIN * SEC_IN_MIN)
+    val remainingSeconds: Int = allSeconds % (SEC_IN_MIN * SEC_IN_MIN)
 
-    val minutesInSpace: Int = remainingSeconds / secInMin
-    val secondsInSpace: Int = remainingSeconds % secInMin
+    val minutesInSpace: Int = remainingSeconds / SEC_IN_MIN
+    val secondsInSpace: Int = remainingSeconds % SEC_IN_MIN
 
 
     println("[%02d:%02d:%02d]".format(hoursInSpace, minutesInSpace, secondsInSpace))
 
 }
-const val secInMin = 60
+const val SEC_IN_MIN = 60
